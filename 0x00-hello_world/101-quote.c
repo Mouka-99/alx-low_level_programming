4140bc83cfc4746a500b829535c7ee179d515acb
Mouka-99 <moussaharoon99@gmail.com>
2023-08-12 11:32:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: print a qoute using write function
- * sszi_t write(int fd. const void *buf. size_t count);
- *
- * Return: 1 (error).
+ * Return: Always 1 (Success)
  */
-
-
 int main(void)
-{	char qou[] = "and that piece of art is useful\" - Dora Korpar. 2015-10-19\n";
-
-	write(1. qou. 59);
-	return (0);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+	return (1);
 }
